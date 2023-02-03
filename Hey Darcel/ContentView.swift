9cca6aabc262b589a90d190c1a4214ef60e4e717
Adam Johnson <adam@adamjohnson.org>
2023-02-03 22:37:37 +1100

    Initial Commit

---
+++ Hey Darcel/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hey Darcel
+//
+//  Created by Adam Johnson on 3/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hey Darcel/Hey_DarcelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hey_DarcelApp.swift
+//  Hey Darcel
+//
+//  Created by Adam Johnson on 3/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hey_DarcelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
