5a72d57d26e78a3a5092c2c0e419eae04d2e9288
Adam Johnson <adam@adamjohnson.org>
2023-03-14 21:49:17 +1100

    Face expressions

--- Hey Darcel/Darcel.swift
+++ Hey Darcel/Darcel.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct Darcel: View {
+    @State private var eyeXPos: CGFloat = 0
+    @State private var eyeYPos: CGFloat = 0
+    @State private var mouthPos = "frown"
+    @State private var expressionTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -22,7 +27,7 @@ struct Darcel: View {
                     .scaledToFit()
                 
                 // Mouth
-                Image("darcel-mouth-smile")
+                Image("darcel-mouth-\(mouthPos)")
                     .resizable()
                     .scaledToFit()
                 
@@ -30,9 +35,57 @@ struct Darcel: View {
                 Image("darcel-eye")
                     .resizable()
                     .scaledToFit()
-                    .offset(y: geometry.size.height / 7.4)
+                    .offset(x: eyeXPos, y: eyeYPos)
+                
+                // Eyelid
+                Image("darcel-eyelid-1")
+                    .resizable()
+                    .scaledToFit()
             }
             .frame(maxWidth: .infinity)
+            .onReceive(expressionTimer) { time in
+                //darcelTimer.upstream.connect().cancel()
+                // Eye
+                let randomEyePos = Int.random(in: 0..<6)
+                let eyeXOffset = 11.8
+                let eyeYOffset = 11.5
+                let eyeXYOffset = 14.8
+                
+                withAnimation(.linear(duration: 0.25)) {
+                    switch randomEyePos {
+                    case 1:
+                        eyeXPos = geometry.size.width / eyeXOffset
+                        eyeYPos = 0
+                    case 2:
+                        eyeXPos = geometry.size.width / eyeXYOffset
+                        eyeYPos = geometry.size.height / eyeXYOffset
+                    case 3:
+                        eyeXPos = 0
+                        eyeYPos = geometry.size.height / eyeYOffset
+                    case 4:
+                        eyeXPos = 0 - (geometry.size.width / eyeXYOffset)
+                        eyeYPos = geometry.size.height / eyeXYOffset
+                    case 5:
+                        eyeXPos = 0 - (geometry.size.width / eyeXOffset)
+                        eyeYPos = 0
+                    default:
+                        eyeXPos = 0
+                        eyeYPos = 0
+                    }
+                    
+                    // Mouth
+                    let randomMouthPos = Int.random(in: 0..<3)
+                    
+                    switch randomMouthPos {
+                    case 1:
+                        mouthPos = "smile"
+                    case 2:
+                        mouthPos = "flat"
+                    default:
+                        mouthPos = "frown"
+                    }
+                }
+            }
         }
     }
 }
